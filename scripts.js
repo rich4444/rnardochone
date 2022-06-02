cfe665963e4e3c66d587105b07beb89a3330dc3e
Ricardo Nardochone <ricardog.n@hotmail.com>
2022-06-01 22:31:19 -0300

    Change Images format and add scroll spy. Removed bubbles

--- scripts.js
+++ scripts.js
@@ -6,20 +6,64 @@ const sunIcon = document.getElementById("sun");
 
 darkButton.addEventListener("click", changeDarkMode)
 
+if(localStorage.getItem("darkTheme") == "light"){
+        changeDarkMode();
+    }
+
 function changeDarkMode (){
     moonIcon.classList.toggle("animout");
     sunIcon.classList.toggle("animout");
     darkTheme.forEach(el => {
         el.classList.toggle("light");
     })
+    localStorage.setItem("darkTheme", darkTheme[0].classList.contains("light") ? "light" : "dark");
 }
 
 
-//Scroll down icon
+//Scroll down icon and back to top button
 const scrollIcon = document.getElementsByClassName("scroll-icon")[0];
+const backButton = document.getElementById("back-button")
 
-window.onscroll = function (e) {
+function hideScrollIcon (){    
     if(window.scrollY > 200){
         scrollIcon.classList.add("scrolled");
+    } if(window.scrollY > 500){        
+        backButton.classList.add("scrolled");
+    } else{
+        backButton.classList.remove("scrolled");
     }
+}
+
+//scroll spy
+const sections = document.querySelectorAll("section");
+const navlinks = document.querySelectorAll("#navbar a");
+
+function scrollSpy (){
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if(top >= offset-150 && top < offset+height-150){
+            navlinks.forEach(link => {
+                let ref = link.getAttribute("href");
+                if(ref == id){
+                    link.classList.add("highlight");
+                }else{
+                    link.classList.remove("highlight");
+                }
+            })
+        }
+    })
+    if(window.scrollY < 300){
+        navlinks.forEach(link => link.classList.remove("highlight"));
+    }
+}
+
+window.onscroll = () => {
+    hideScrollIcon();
+    scrollSpy();
 };
+
+
