83f960e8d3c45b2de31c95d263c0280aa08f5cba
Ricardo Nardochone <ricardog.n@hotmail.com>
2022-06-04 05:10:12 -0300

    Back to fix 4 best working so far

--- scripts.js
+++ scripts.js
@@ -167,13 +167,13 @@ function closeNavBar() {
 
 // // Fix url bar
 // // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-// let vh = window.innerHeight * 0.01;
+let vh = window.innerHeight * 0.01;
 // // Then we set the value in the --vh custom property to the root of the document
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 // // We listen to the resize event
-// window.addEventListener('resize', () => {
-//   // We execute the same script as before
-//   let vh = window.innerHeight * 0.01;
-//   document.documentElement.style.setProperty('--vh', `${vh}px`);
-// });
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
